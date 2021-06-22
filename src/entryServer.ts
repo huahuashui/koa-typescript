d2a62f193116a5a97279842b5c98d06f05d8563e
tan <jie.tan@iask.cn>
2021-06-22 17:45:32 +0800

    1、忽略目录 2、静态资源暴露

--- src/entryServer.ts
+++ src/entryServer.ts
@@ -5,12 +5,25 @@ import helmet from 'koa-helmet';
 import RouterMain from "./router";
 import interceptorMain from "./interceptor";
 import Config from "../config";
+import koaStatic from "koa-static";
+import favicon from "koa-favicon";
 
 function linkStart() {
     // 创建一个Koa对象表示web app本身:
     const app = new Koa();
     const router = new Router();
 
+    // 静态资源暴露
+    app.use(koaStatic('./static', {
+        // 默认为true  访问的文件为index.html  可以修改为别的文件名或者false
+        index: false,
+        // 是否同意传输隐藏文件
+        hidden: false,
+        // 如果为true，则在返回next()之后进行服务，从而允许后续中间件先进行响应
+        defer: true,
+    }));
+    // 设置图标
+    app.use(favicon('/images/favicon.ico'));
     // 安全处理
     app.use(helmet());
     // 处理请求体
